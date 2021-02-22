e1f4caf3d05452c4a3cb581051a31d082462f29f
Oleh Momot <oleh_momot@pm.me>
2021-02-22 12:22:18 +0200

    fix: export types of PluginManager

--- src/browser.ts
+++ src/browser.ts
@@ -1,2 +1,4 @@
 export * from './plugins/browser.types';
+export * from './pluginManager/browser.types';
+
 export {default as PluginManager} from './pluginManager/browser';
--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,4 @@
 export * from './plugins/server.types';
+export * from './pluginManager/server.types';
+
 export {default as PluginManager} from './PluginManager/server';
